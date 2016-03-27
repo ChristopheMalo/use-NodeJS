5d1eaddc0564f10652f94cbd1525c8d597205c34
ChristopheMalo <christof.malo@gmail.com>
2016-03-27 16:22:23 +0200

    feat: return valid HTML

--- first-app/js/server.js
+++ first-app/js/server.js
@@ -11,6 +11,18 @@ var http = require('http'); // Call http Node.js lib to create HTTP Server
 
 var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.end('<p>Hello World! My first Node.js Server - <strong>Code with HTML</strong></p>');
+    response.write(
+        '<!DOCTYPE html>' +
+        '<html lang="en">' +
+        '    <head>' +
+        '       <meta charset="utf-8" />' +
+        '       <title>Ma first Node.js page !</title>' +
+        '    </head>' +
+        '    <body>' +
+        '       <p>Hello World! My first Node.js Server - <strong>Code with HTML</strong></p>' +
+        '    </body>' +
+        '</html>'
+    );
+    response.end();
 });
 server.listen(8080); // Start the server
