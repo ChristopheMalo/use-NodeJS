0cbca7ba4a912c41cf2e0d5ac42eecb5116a334f
ChristopheMalo <christof.malo@gmail.com>
2016-03-27 15:35:56 +0200

    feat: code hello-world

---
+++ js/01_hello-world.js
@@ -0,0 +1,11 @@
+/**
+ * A simple file to test the famous hello world !
+ * First program in all language
+ * 
+ * Simply use node js/01_hell-world.js to run the file in console
+ * Display sentence in console
+ * 
+ * @author: Christophe Malo
+ * @version: 1.0.0
+ */
+console.log('Hello World! Welcome in Node.js');
