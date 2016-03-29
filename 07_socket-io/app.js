9433bd99703699465f16e9737e3359b639a6eda7
ChristopheMalo <christof.malo@gmail.com>
2016-03-29 11:05:51 +0200

    feat: use socket.io in app

---
+++ 07_socket-io/app.js
@@ -0,0 +1,29 @@
+/**
+ * Test socket.io in application
+ * 
+ * @author: Christophe Malo
+ * @version: 0.1.0
+ */
+var http = require('http');
+var fs   = require('fs'); // Call module File System
+
+// Load index file - this file is displayed to client
+var server = http.createServer(function (request, response)
+{
+    fs.readFile('./index.html', 'utf-8', function(error, content)
+    {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.end(content);
+    });
+});
+
+// Load socket.io
+var socketio = require('socket.io').listen(server);
+
+// Display client connection in console
+socketio.sockets.on('connection', function(socket)
+{
+    console.log('A client is connected');
+});
+
+server.listen(3000);
